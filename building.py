eee9f96e60caad22a5242526cbaa74e92948a12a
AKAfreaky <frogman.jo@gmail.com>
2014-03-09 01:37:45 +0000

    Switched to the general build method
    Also re-ordered methods and messed with comments

--- building.py
+++ building.py
@@ -8,6 +8,9 @@ from POSH.jython_compat import *
 # behaviour base for POSH behaviours
 from POSH import Behaviour
 
+# enum for unit types
+from jnibwapi.types.UnitType import UnitTypes
+
 class Building(Behaviour):
     """ Contains behaviours and senses that are concerned with the placement
     of buildings """
@@ -40,16 +43,63 @@ class Building(Behaviour):
                             "forge_count", "completed_forge_count", "free_forge_count"
                             ))
     
+    '''
+    == Zerg Behaviours ==
+    '''
+    
     def build_macro_hatchery(self):
         self.log.info("Attempting to build a macro hatchery")
-        return self.agent.BWBot.bot.buildingManager.buildMacroHatchery()
-    
-    def hatchery_count(self):
-        return self.agent.BWBot.bot.buildingManager.getHatcheryCount()
+        return self.agent.BWBot.bot.buildingManager.buildBuilding( UnitTypes.Zerg_Hatchery )
     
     def build_spawning_pool(self):
         self.log.info("Attempting to build spawning pool")
-        return self.agent.BWBot.bot.buildingManager.buildSpawningPool()
+        return self.agent.BWBot.bot.buildingManager.buildBuilding( UnitTypes.Zerg_Spawning_Pool )
+    
+    def build_hydralisk_den(self):
+        self.log.info("Attempting to build hydralisk den")
+        return self.agent.BWBot.bot.buildingManager.buildBuilding( UnitTypes.Zerg_Hydralisk_Den )
+    
+    def build_evolution_chamber(self):
+        self.log.info("Attempting to build evolution chamber")
+        return self.agent.BWBot.bot.buildingManager.buildBuilding( UnitTypes.Zerg_Evolution_Chamber )
+    
+    def build_creep_colony(self):
+        self.log.info("Attempting to build creep colony")
+        return self.agent.BWBot.bot.buildingManager.buildBuilding( UnitTypes.Zerg_Creep_Colony )
+    
+    def upgrade_to_sunken(self):
+        self.log.info("Attempting to upgrade to sunken colony")
+        return self.agent.BWBot.bot.buildingManager.upgradeSunkenColony()
+    
+    # Sends a drone to morph into an extractor
+    def build_extractor(self):
+        self.log.info("Attempting to build extractor")
+        return self.agent.BWBot.bot.buildingManager.buildExtractor()
+    
+    # Building expansions, need to do these in order:
+    def send_drone_expansion(self):
+        return self.agent.BWBot.bot.buildingManager.sendWorkerToExpansionLocation()
+    
+    # No longer needed, now worker will automatically start building once it reaches the location
+    def build_expansion_hatchery(self):
+        self.log.info("building expansion ")
+        return_value = self.agent.BWBot.bot.buildingManager.buildExpansionHatchery()
+        self.log.info("building expansion returned " + str(return_value) )
+        return return_value
+    
+    def build_spire(self):
+        self.log.info("Attempting to build spire")
+        return self.agent.BWBot.bot.buildingManager.buildSpire()
+    
+    def upgrade_to_lair(self):
+        return self.agent.BWBot.bot.buildingManager.upgradeToLair()
+    
+    '''
+    == Zerg Senses ==
+    '''
+    
+    def hatchery_count(self):
+        return self.agent.BWBot.bot.buildingManager.getHatcheryCount()
     
     def has_spawning_pool(self):
         return self.agent.BWBot.bot.buildingManager.hasSpawningPool(False)
@@ -57,27 +107,11 @@ class Building(Behaviour):
     def has_completed_spawning_pool(self):
         return self.agent.BWBot.bot.buildingManager.hasSpawningPool(True)
     
-    def build_hydralisk_den(self):
-        self.log.info("Attempting to build hydralisk den")
-        return self.agent.BWBot.bot.buildingManager.buildHydraliskDen()
-    
-    def build_evolution_chamber(self):
-        self.log.info("Attempting to build evolution chamber")
-        return self.agent.BWBot.bot.buildingManager.buildEvolutionChamber()
-    
     def evo_chamber_count(self):
         return self.agent.BWBot.bot.buildingManager.getEvolutionChamberCount(False)
     
     def completed_evo_chamber_count(self):
         return self.agent.BWBot.bot.buildingManager.getEvolutionChamberCount(True)
-    
-    def build_creep_colony(self):
-        self.log.info("Attempting to build creep colony")
-        return self.agent.BWBot.bot.buildingManager.buildCreepColony()
-    
-    def upgrade_to_sunken(self):
-        self.log.info("Attempting to upgrade to sunken colony")
-        return self.agent.BWBot.bot.buildingManager.upgradeSunkenColony()
     
     def colony_count(self):
         return self.agent.BWBot.bot.buildingManager.getCompletedColonyCount()
@@ -93,11 +127,6 @@ class Building(Behaviour):
     
     def has_completed_hydralisk_den(self):
         return self.agent.BWBot.bot.buildingManager.hasHydraliskDen(True)
-    
-    # Sends a drone to morph into an extractor
-    def build_extractor(self):
-        self.log.info("Attempting to build extractor")
-        return self.agent.BWBot.bot.buildingManager.buildExtractor()
     
     # Do we have a completed extractor
     def has_completed_extractor(self):
@@ -119,40 +148,18 @@ class Building(Behaviour):
     def has_extractor_saturation(self):
         return self.agent.BWBot.bot.buildingManager.hasExtractorSaturation()
     
-    # Building expansions, need to do these in order:
-    def send_drone_expansion(self):
-        return self.agent.BWBot.bot.buildingManager.sendWorkerToExpansionLocation()
-    
     # Once send_drone_expansion has been called, check for expansionDroneReady before building expansion
     def check_drone_ready_expand(self):
-        return_value = self.agent.BWBot.bot.buildingManager.expansionDroneReady()
-        #if(return_value == True):
-            #self.log.info("drone ready to build expansion")
-        #else:
-            #self.log.info("drone not ready to build expansion")
-        return return_value
-    
-    def build_expansion_hatchery(self):
-        self.log.info("building expansion ")
-        return_value = self.agent.BWBot.bot.buildingManager.buildExpansionHatchery()
-        self.log.info("building expansion returned " + str(return_value) )
-        return return_value
+        return self.agent.BWBot.bot.buildingManager.expansionDroneReady()
     
     def expansion_count(self):
         return self.agent.BWBot.bot.buildingManager.getExpansionCount()
     
-    def build_spire(self):
-        self.log.info("Attempting to build spire")
-        return self.agent.BWBot.bot.buildingManager.buildSpire()
-     
     def has_spire(self):
         return self.agent.BWBot.bot.buildingManager.hasSpire(False)
     
     def has_completed_spire(self):
         return self.agent.BWBot.bot.buildingManager.hasSpire(True)
-    
-    def upgrade_to_lair(self):
-        return self.agent.BWBot.bot.buildingManager.upgradeToLair()
     
     def has_lair(self):
         return self.agent.BWBot.bot.buildingManager.hasLair(False)
@@ -160,27 +167,43 @@ class Building(Behaviour):
     def has_completed_lair(self):
         return self.agent.BWBot.bot.buildingManager.hasLair(True)
     
-    # Terran Build commands
+    '''
+    == Terran Behaviours ==
+    '''
     
     def build_supply_depot(self):
-        return self.agent.BWBot.bot.buildingManager.buildSupplyDepot()
+         return self.agent.BWBot.bot.buildingManager.buildBuilding( UnitTypes.Terran_Supply_Depot )
     
     def build_barracks(self):
-        return self.agent.BWBot.bot.buildingManager.buildBarracks()
+        return self.agent.BWBot.bot.buildingManager.buildBuilding( UnitTypes.Terran_Barracks )
+    
+    '''
+    == Terran Senses ==
+    '''
     
     def barracks_count(self):
         return self.agent.BWBot.bot.buildingManager.getBarracksCount()
     
-    # Protoss Build commands
+    '''
+    == Protoss Behaviours ==
+    '''
     
     def build_pylon(self):
-        return self.agent.BWBot.bot.buildingManager.buildPylon()
+        return self.agent.BWBot.bot.buildingManager.buildBuilding( UnitTypes.Protoss_Pylon )
     
+    def build_gateway(self):
+        return self.agent.BWBot.bot.buildingManager.buildBuilding( UnitTypes.Protoss_Gateway )
+    
+    def build_forge(self):
+        return self.agent.BWBot.bot.buildingManager.buildBuilding( UnitTypes.Protoss_Forge )
+    
+    '''
+    == Protoss Senses ==
+    '''
     def pylon_count(self):
         return self.agent.BWBot.bot.buildingManager.getPylonCount()
     
-    def build_gateway(self):
-        return self.agent.BWBot.bot.buildingManager.buildGateway()
+    
     
     def gateway_count(self):
         return self.agent.BWBot.bot.buildingManager.getGatewayCount()
@@ -188,8 +211,7 @@ class Building(Behaviour):
     def completed_gateway_count(self):
         return self.agent.BWBot.bot.buildingManager.getCompletedGatewayCount()
     
-    def build_forge(self):
-        return self.agent.BWBot.bot.buildingManager.buildForge()
+    
     
     def forge_count(self):
         return self.agent.BWBot.bot.buildingManager.getForgeCount()
